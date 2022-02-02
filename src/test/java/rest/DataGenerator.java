e0c955202d0c8aff23bdb62fa67602a7e98fb306
Sapozhnikova Ksenia <mobillka92@gmail.com>
2022-02-03 02:19:00 +0300

    test datagenerator added

--- src/test/java/rest/DataGenerator.java
+++ src/test/java/rest/DataGenerator.java
@@ -1,5 +1,62 @@
 package rest;
 
-public class DataGenerator {
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    public static class RegistrationInfo {
+        private RegistrationInfo() {
+        }
+
+        public static void setUpUser(Form user) {
+            // сам запрос
+            given() // "дано"
+                    .spec(requestSpec) // указываем, какую спецификацию используем
+                    .body(user)
+                    .when() // "когда"
+                    .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                    .then() // "тогда ожидаем"
+                    .statusCode(200); // код 200 OK
+        }
+
+        public static String makeUserPassword(String Locale) {
+            Faker faker = new Faker(new Locale(Locale));
+            return faker.internet().password();
+        }
+
+        public static String makeUserName(String Locale) {
+            Faker faker = new Faker(new Locale(Locale));
+            return faker.name().username();
+        }
+
+        public static String setUserStatus(String status) {
+            return status;
+        }
+
+        public static Form generateUserInfo(String Locale, String userStatus) {
+            return new Form(
+                    makeUserName(Locale),
+                    makeUserPassword(Locale),
+                    setUserStatus(userStatus)
+            );
+        }
+    }
 }
--- src/test/java/rest/TestClass.java
+++ src/test/java/rest/TestClass.java
@@ -1,4 +1,63 @@
 package rest;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static rest.DataGenerator.RegistrationInfo.setUpUser;
+
+
 public class TestClass {
+
+    @BeforeEach
+    void headlessMode() {
+        Configuration.headless = true;
+    }
+
+    private void loginForm(String login, String password) {
+        open("http://localhost:9999");
+        $("[data-test-id=login] input").setValue(login);
+        $("[data-test-id=password] input").setValue(password);
+        $("[data-test-id=action-login]").click();
+    }
+
+    @Test
+    public void shouldLoginExistsActiveUser() {
+        Form user = DataGenerator.RegistrationInfo.generateUserInfo("ru", "active");
+        setUpUser(user);
+        loginForm(user.getLogin(), user.getPassword());
+        $(byText("Личный кабинет")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void shouldNotLoginExistsBlockedUser() {
+        Form user = DataGenerator.RegistrationInfo.generateUserInfo("ru", "blocked");
+        setUpUser(user);
+        loginForm(user.getLogin(), user.getPassword());
+
+        $(withText("Пользователь заблокирован")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void shouldNotLoginWithWrongUsername() {
+        Form user = DataGenerator.RegistrationInfo.generateUserInfo("ru", "active");
+        setUpUser(user);
+        loginForm(DataGenerator.RegistrationInfo.makeUserName("ru"), user.getPassword());
+        $(withText("Неверно указан логин или пароль")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void shouldNotLoginWithWrongPassword() {
+        Form user = DataGenerator.RegistrationInfo.generateUserInfo("ru", "active");
+        setUpUser(user);
+        loginForm(user.getLogin(), DataGenerator.RegistrationInfo.makeUserPassword("ru"));
+        $(withText("Неверно указан логин или пароль")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
 }
