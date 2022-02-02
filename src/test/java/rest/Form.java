9defaf72ce28626d1b8e08e19d38783475c42bf5
Sapozhnikova Ksenia <mobillka92@gmail.com>
2022-02-03 00:42:59 +0300

    init

---
+++ src/test/java/rest/Form.java
@@ -0,0 +1,14 @@
+package rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Form {
+    private String login;
+    private String password;
+    private String status;
+}
